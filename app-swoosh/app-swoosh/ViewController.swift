c611d42ef914de157b15e8332b8b5b1476c9ca00
Den <denysdiachenko1997@gmail.com>
2020-01-19 15:08:21 +0200

    Second Lesson

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var BGimage: UIImageView!
+    
 
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+          super.viewDidLoad()
     }
-
-
+    
+    
+    
 }
 
