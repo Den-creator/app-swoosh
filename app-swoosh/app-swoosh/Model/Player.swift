4e8f6f7dda815414e60319b5209201b8b8b2ad4c
Den <denysdiachenko1997@gmail.com>
2020-01-20 18:49:25 +0200

    lesson-07

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,24 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ден on 20.01.2020.
+//  Copyright © 2020 Ден. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+        
+    
+    
+    
+    
+    
+    
+    
+}
